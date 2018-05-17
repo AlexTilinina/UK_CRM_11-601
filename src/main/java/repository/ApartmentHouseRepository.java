8783ca57e597c56275c9bf61b08825cce7e4a7bf
Tilinina Alexandra <Sasha_til@mail.ru>
2018-05-18 00:22:15 +0300

    R6, R19, R20 added Spring Security, R13 added forms, R16, R17, R18 moved + spring boot

--- src/main/java/repository/ApartmentHouseRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.ApartmentsHouse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApartmentHouseRepository extends JpaRepository<ApartmentsHouse, Long> {
-}
--- src/main/java/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
